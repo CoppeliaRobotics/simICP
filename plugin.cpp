7999a92386ef1fc000ee4fea63063bfb97efbd67
Federico Ferri <federico.ferri.it@gmail.com>
2018-06-10 14:27:14 +0200

    add module info

--- plugin.cpp
+++ plugin.cpp
@@ -60,6 +60,9 @@ public:
     {
         if(!registerScriptStuff())
             throw std::runtime_error("failed to register script stuff");
+
+        simSetModuleInfo(PLUGIN_NAME, 0, "ICP (iterative closest point algorithm) Plugin", 0);
+        simSetModuleInfo(PLUGIN_NAME, 1, __DATE__, 0);
     }
 };
 
